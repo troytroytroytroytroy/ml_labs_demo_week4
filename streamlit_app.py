7a30ec6f48bd8f6d656b0d2081f2c2bf772ea83e
Troy <99522481+troytroytroytroytroy@users.noreply.github.com>
2024-08-26 02:53:02 +0000

    f

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,12 +34,7 @@ example = pd.DataFrame({
     "instant_bookable": [instant_bookable]
     })
 
-example2 = pd.DataFrame({
-    "review_scores_rating": [0.50],
-    "room_type": ["Shared room"],
-    "service_cost": ["$0.99"],
-    "instant_bookable": [0]
-    })
+
 
 
 if st.button('Predict?'):
